af2fe6db1b3eea4dad742fb85827933934e72ba0
XGenietony <42089807+XGenietony@users.noreply.github.com>
2021-11-29 17:47:10 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,4 +3,4 @@ conda create -n PointMVSNet python=3.6
 source activate PointMVSNet
 conda install -y pytorch==1.0.1 torchvision==0.2.2 cudatoolkit=9.0 -c pytorch
 conda install -c anaconda pillow
-pip install -r requirements
+#pip install -r requirements
